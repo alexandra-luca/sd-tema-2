1bd9cebe9f39849d071d0d1a1c6531a4f60bf6d8
Alexandra Luca <alexandra.luca1611@gmail.com>
2020-04-29 22:44:16 +0300

    delete commented code

--- main.c
+++ main.c
@@ -405,8 +405,6 @@ int main(int argc, char *argv[])
 		p = strtok(linie, " ");
 		strcpy(comanda, p);
 
-		//printf("%s\n", comanda);
-
 		if (strcmp(comanda, "set_band") == 0) {
 			// 4.1
 			p = strtok(NULL, " ");
@@ -472,59 +470,7 @@ int main(int argc, char *argv[])
 
 			BrowserWait(abrowser, x);
 		}
-	}	
-
-	/*
-
-	// TESTING QUEUE ----------------------------------------------
-
-	AQ aq = InitQ(sizeof(int));
-	int x;
-
-	x = 15;
-	InsertQ(aq, &x);
-	x = 27;
-	InsertQ(aq, &x);
-	x = 100;
-	InsertQ(aq, &x);
-
-	int *p;
-
-	p = PeakQ(aq);
-	printf("%d should be 15 \n", *p);
-	p = ExtractQ(aq);
-	printf("%d // 15 \n", *p);
-	p = ExtractQ(aq);
-	printf("%d // 27 \n", *p);
-	p = ExtractQ(aq);
-	printf("%d // 100 \n", *p);
-	p = ExtractQ(aq);
-	printf("%p should be nil \n", p);
-
-	// TESTING STACK -----------------------------------------------
-
-	AS as = InitS(sizeof(int));
-
-	x = 30;
-	Push(as, &x);
-	x = 42;
-	Push(as, &x);
-	x = 1000;
-	Push(as, &x);
-
-	p = Top(as);
-	printf("%d // 1000\n", *p);
-	p = Pop(as);
-	printf("%d // 1000\n", *p);
-	p = Pop(as);
-	printf("%d // 42\n", *p);
-	p = Pop(as);
-	printf("%d // 30\n", *p);
-	p = Pop(as);
-	printf("%p // nil\n", p);
-
-	// ------------------------------------------------------------
-	*/
-	
+	}
+		
 	return 0;
 }
